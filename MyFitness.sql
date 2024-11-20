c4610aed48fc978b6435b933e0aaf6ffbe2b3863
Ivan <ivalisleo@alu.edu.gva.es>
2024-11-20 14:39:47 +0100

    Añadida base de datos

---
+++ MyFitness.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost
+-- Tiempo de generación: 20-11-2024 a las 14:08:04
+-- Versión del servidor: 10.4.11-MariaDB
+-- Versión de PHP: 7.4.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `MyFitness`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `idCarrito` int(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `dniCliente` varchar(9) COLLATE utf8_unicode_ci NOT NULL,
+  `nombre` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(75) COLLATE utf8_unicode_ci NOT NULL,
+  `contrasena` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `apellidos` text COLLATE utf8_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `clientes`
+--
+
+INSERT INTO `clientes` (`dniCliente`, `nombre`, `email`, `contrasena`, `apellidos`) VALUES
+('15', 'Laura', 'admin@gmail.com', 'Laura', 'false'),
+('1', 'Admin', 'admin@midominio.es', 'Admin\r\n', 'true'),
+('7777777', 'Miguel', 'manuel@midominio.es', 'Miguel', 'false'),
+('44444444', 'Marta', 'marta@midominio.es', 'Marta', 'false'),
+('10', 'Victor', 'victor@midominio.es', 'Victor', 'false');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `lineasCarrito`
+--
+
+CREATE TABLE `lineasCarrito` (
+  `idCarrito` int(4) NOT NULL,
+  `nlinea` int(2) NOT NULL,
+  `idProducto` int(6) DEFAULT NULL,
+  `cantidad` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `idProducto` int(6) NOT NULL,
+  `nombre` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `foto` longblob DEFAULT NULL,
+  `peso` int(3) NOT NULL,
+  `precio` int(4) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`idCarrito`);
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indices de la tabla `lineasCarrito`
+--
+ALTER TABLE `lineasCarrito`
+  ADD PRIMARY KEY (`idCarrito`,`nlinea`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `idProducto` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
