7d3a2941c25411a1d37c6d7d3ebe97d7f611349f
Ivan <ivalisleo@alu.edu.gva.es>
2024-11-20 15:43:43 +0100

    Realizadas inserciones en la base de datos

--- MyFitness.sql
+++ MyFitness.sql
@@ -7,6 +7,9 @@
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.6
 
+create database MyFitness;
+use MyFitness;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
